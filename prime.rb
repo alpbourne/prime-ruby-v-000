1a5bbd216118aa56b536c3787668b891fe340e75
alpbourne <alpbourne@gmail.com>
2017-09-21 00:21:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(i)
   prime_range_test = (2..i-1).to_a
-  if prime_range_test.any? {|i| integer % i == 0 }
+  if prime_range_test.any? {|number| i % number == 0 }
     return false
   elsif i <= 1
     return false
